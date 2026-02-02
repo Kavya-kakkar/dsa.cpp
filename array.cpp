cd259b8b1933d885fc2b046e53328218297fbb35
Kavya <kavyakakkar234@gmail.com>
2026-02-03 00:05:01 +0530

    array.cppadded

--- array.cpp
+++ array.cpp
@@ -1,18 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <climits>
 using namespace std;
-int main(){
-    int nums[]={12,-15,-23,7,24,-2};
-    int size= 6;
 
-    int smallest =INT_MAX;
-    int largest = INT_;
-    for (int i=0; i<size ; i++){
-        smallest =min(nums[i],smallest);
-        largest =max(nums[i],largest);
+int main() {
+    int nums[] = {12, -15, -23, 7, 24, -2};
+    int size = 6;
+
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+
+    for (int i = 0; i < size; i++) {
+        smallest = min(nums[i], smallest);
+        largest = max(nums[i], largest);
     }
-    cout<<"smallest ="<<smallest<<endl;
-    cout<<"largest ="<<largest<<endl;
+
+    cout << "smallest = " << smallest << endl;
+    cout << "largest = " << largest << endl;
+
     return 0;
-
-    }
+}
 
